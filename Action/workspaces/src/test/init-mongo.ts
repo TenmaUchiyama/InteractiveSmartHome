75bbaf7bf2bbeff7446e1102596ccbb951848bef
Tenma <tenmaimp@gmail.com>
2024-08-08 10:38:23 +0900

    devcontainer編集 prettierの追加 mongodbの起動

---
+++ Action/workspaces/src/test/init-mongo.ts
@@ -0,0 +1,15 @@
+import {MongoClient} from 'mongodb'
+
+
+let client = new MongoClient('mongodb://mongodb:27017')
+
+
+
+const main = async () => {
+  try {  await client.connect()}catch{console.error("not working")}finally{await client.close()}
+
+
+}
+
+
+main()
