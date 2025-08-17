2288f88b322804861b488c1af810d36017a1f49b
Tenma <tenmaimp@gmail.com>
2025-08-17 11:29:46 +0900

    Result

--- LLMServer/workspace/server.py
+++ LLMServer/workspace/server.py
@@ -24,7 +24,7 @@ from agents.pointing_agent.point_node import change_pointing_model
 from utils.time_tracker import TimeTracker
 
 
-OUTPUT_FILE_NAME="SystemEvaluation"  # Change this to your desired output file name
+OUTPUT_FILE_NAME="P14" # Change this to your desired output file name
 app = FastAPI()
 
 
